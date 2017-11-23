45217ef5c07e472ec951274f7368b6f4062a674c
mercuryglory <wzhseu@163.com>
2017-11-23 20:07:24 +0800

    add dependency

--- app/src/main/java/com/mercury/sophixdemo/Main2Activity.java
+++ app/src/main/java/com/mercury/sophixdemo/Main2Activity.java
@@ -1,13 +1,51 @@
 package com.mercury.sophixdemo;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.bumptech.glide.Glide;
+
+import butterknife.Bind;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
 
 public class Main2Activity extends AppCompatActivity {
+
+    @Bind(R.id.tv_content)
+    TextView  tvContent;
+    @Bind(R.id.btn_dependency)
+    Button    btnDependency;
+    @Bind(R.id.iv)
+    ImageView iv;
+    @Bind(R.id.tv_version)
+    TextView tvVersion;
+
+    private static final String IMAGE_URL = "http://img.ivsky.com/img/tupian/li/201708/23/yingyangfengfudeheimeishuiguotupian-005.jpg";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main2);
+        ButterKnife.bind(this);
+
+        tvContent.setText(PropertyUtil.getInstance().readCofigFile());
+
+    }
+
+    @OnClick({R.id.tv_content, R.id.btn_dependency})
+    public void onClick(View view) {
+        switch (view.getId()) {
+            case R.id.tv_content:
+                ToastUtil.showToast(Main2Activity.this, "新增了资源目录和文件");
+                break;
+            case R.id.btn_dependency:
+                Glide.with(Main2Activity.this).load(IMAGE_URL).into(iv);
+
+                break;
+        }
     }
 }
---
+++ app/src/main/java/com/mercury/sophixdemo/PropertyUtil.java
@@ -0,0 +1,44 @@
+package com.mercury.sophixdemo;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Properties;
+
+/**
+ * Created by wang.zhonghao on 2017/11/22.
+ * description:  新增的类
+ */
+
+public class PropertyUtil {
+
+    private static PropertyUtil instance;
+    private Properties mProperties;
+
+    public static PropertyUtil getInstance() {
+        if (instance == null) {
+            synchronized (PropertyUtil.class) {
+                if (instance == null) {
+                    instance = new PropertyUtil();
+                }
+            }
+        }
+        return instance;
+    }
+
+    private PropertyUtil() {
+        mProperties= new Properties();
+        InputStream inputStream = MyApplication.context.getResources().openRawResource(R.raw
+                .config);
+        try {
+            mProperties.load(inputStream);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+    }
+
+    public String readCofigFile() {
+        return mProperties.getProperty("content", "");
+
+    }
+}
