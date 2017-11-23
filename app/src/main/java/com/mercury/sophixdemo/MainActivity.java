1d8e174dcf6f7008a3bc0af3499c0017b3a9ba7c
mercuryglory <wzhseu@163.com>
2017-11-23 18:24:39 +0800

    Feat:add variables and methods in existed class,including static and non-static

--- app/src/main/java/com/mercury/sophixdemo/MainActivity.java
+++ app/src/main/java/com/mercury/sophixdemo/MainActivity.java
@@ -6,13 +6,24 @@ import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
+    private int param = 1;
+    private static int paramsStatic = 2;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
         TextView textView = (TextView) findViewById(R.id.text);
-        textView.setText("热修复成功了,这是第一次");
+        textView.setText("热修复成功了,这是第" + getMulti(param, paramsStatic) + "次");
 
+    }
+
+    private int getCount(int a, int b) {
+        return a + b;
+    }
+
+    private static int getMulti(int a, int b) {
+        return a * b;
     }
 }
