91bfbc76d7895af0bec89f016353546344d3558c
mercuryglory <wzhseu@163.com>
2017-11-23 18:22:50 +0800

    Feat:modified method in class,simplest

--- app/src/main/java/com/mercury/sophixdemo/MainActivity.java
+++ app/src/main/java/com/mercury/sophixdemo/MainActivity.java
@@ -2,6 +2,7 @@ package com.mercury.sophixdemo;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,6 +10,9 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        TextView textView = (TextView) findViewById(R.id.text);
+        textView.setText("热修复成功了,这是第一次");
 
     }
 }
